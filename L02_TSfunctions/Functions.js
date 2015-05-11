86859ecb0192c65b6208410c5a0c52beeb6b6351
cashwu <cash@cashwu.com>
2015-05-11 21:57:39 +0800

    ch01 bast type

---
+++ L02_TSfunctions/Functions.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cash on 2015/05/11.
+ */
+function attr(nameorage) {
+    if (nameorage && typeof nameorage === "string") {
+        alert("姓名");
+    }
+    else {
+        alert("年齡");
+    }
+}
+attr("hello");
+attr(10);
+//# sourceMappingURL=Functions.js.map
