ca1e3335d2d5c5b2e65a01f539333485a4f35616
cashwu <cash@cashwu.com>
2015-05-12 09:46:05 +0800

    ch04 interface

---
+++ L05_Interfaces/Interfaces.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cash on 2015/05/12.
+ */
+var s = {};
+s.color = "blue";
+s.sideLength = 10;
+s.penWindth = 11;
+var c;
+c(10);
+c.reset();
+//# sourceMappingURL=Interfaces.js.map
